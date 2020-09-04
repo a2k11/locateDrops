7925e828ac0d97a3cd87f9dc36ae92789bdacd5f
Adam Kaewell <adam.kaewell@gmail.com>
2020-09-03 23:55:08 -0400

    authenticating users from apollo server, adding context to apollo server for users

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { GraphQLClient } from 'graphql-request'
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+const MY_QUERY = `
+{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+}
+`
+
 const Login = ({ classes }) => {
 
-  const onSuccess = googleUser => {
+  const onSuccess = async googleUser => {
     const idToken = googleUser.getAuthResponse().id_token
-    console.log(idToken);
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+    })
+    const data = await client.request(MY_QUERY)
+    console.log({ data })
   }
 
   return (
