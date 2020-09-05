0fcc9c41865f692cdedf6bc949f7cd6d8e41ca60
a2k11 <akaewell@xometry.com>
2020-09-04 23:58:12 -0400

    setting up verifying user, setting up auth token, checking to create user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,31 @@
 const User = require('../models/User')
 const { OAuth2Client } = require('google-auth-library')
-new OAuth2Client(process.env.OAUTH_CLIENT_ID)
+const client = new OAuth2Client(process.env.OAUTH_CLIENT_ID)
+
+exports.findOrCreateUser = async token => {
+    // verify auth token, check user exists, return user or create 
+    const googleUser = await verifyAuthToken(token)
+    const user = await checkIfUserExists(googleUser.email)
+    return user ? user : createNewUser(googleUser)
+
+}
+
+const verifyAuthToken = async token => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.OAUTH_CLIENT_ID
+        })
+        return ticket.getPayload()
+    } catch (err) {
+        console.error("Error verifying auth token", err)
+    }
+}
+
+const checkIfUserExists = async email => await (await User.findOne({ email })).exec()
+
+const createNewUser = googleUser => {
+    const { name, email, picture } = googleUser
+    const user = { name, email, picture }
+    return new User(user).save()
+}
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const { findOrCreateUser } = require('./controllers/userController')
+const { isNullableType } = require('graphql')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("db connects"))
@@ -12,16 +14,18 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => {
+  context: async ({ req }) => {
     let authToken = null
+    let currentUser = isNullableType
     try {
       authToken = req.headers.authorization
       if (authToken) {
-        // find or create user
+        currentUser = await findOrCreateUser(authToken)
       }
     } catch (err) {
       console.error(`Unable to authenticate user with token ${authToken}`, err)
     }
+    return ( currentUser )
   }
 })
 
